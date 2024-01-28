c0b81b6d5e1999e3a94edd0465be9d83af331816
vosw1 <vosw1@naver.com>
2024-01-29 08:55:21 +0900

    ígit 동기화

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BoardController { //main을 바로 호출하는 컨트롤러
+public class BoardController { // git main을 바로 호출하는 컨트롤러
 
     @GetMapping("/main")
     public String main() {
